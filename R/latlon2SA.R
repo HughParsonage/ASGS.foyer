25bf2a9b0e87fb76453cc42045431b629f3d04e0
HughParsonage <hugh.parsonage@gmail.com>
2018-03-21 11:51:29 +1100

    Add .shapefile argument

--- R/latlon2SA.R
+++ R/latlon2SA.R
@@ -4,6 +4,7 @@
 #' @param yr The year of the statistical area.
 #' @param return Whether to return an atomic vector \code{(v)} representing the shapefile for each point \code{lat, lon} or a spatial points object from package \code{sp}.
 #' @param NAME (logical, default: \code{TRUE}) whether to use the name or number of the statistical area
+#' @param .shapefile If specified, an arbitrary shapefile containing the statistical areas to locate.
 #' @return The statistical area that contains each point.
 #' @examples
 #' latlon2SA(-35.3, 149.2, to = "STE", yr = "2016")
@@ -15,28 +16,35 @@ latlon2SA <- function(lat,
                       to = c("STE", "SA2", "SA1", "SA3", "SA4"),
                       yr = c("2016", "2011"),
                       return = c("v", "sp"),
-                      NAME = TRUE) {
-  # Could use NSE but can't be arsed:
-  to <- match.arg(to)
+                      NAME = TRUE,
+                      .shapefile = NULL) {
+  if (is.null(.shapefile)) {
+    # Could use NSE but can't be arsed:
+    to <- match.arg(to)
 
-  if (to != "STE" && !isNamespaceLoaded("ASGS")) {
-    stop("The ASGS package is required for any statistical area besides 2016")
-  }
+    if (to != "STE" && !isNamespaceLoaded("ASGS")) {
+      stop("The ASGS package is required for any statistical area besides 2016")
+    }
 
-  yr <- match.arg(yr)
-  if (yr != "2016" && !isNamespaceLoaded("ASGS")) {
-    stop("The ASGS package is required for `year = 2011`.")
-  }
+    yr <- match.arg(yr)
+    if (yr != "2016" && !isNamespaceLoaded("ASGS")) {
+      stop("The ASGS package is required for `year = 2011`.")
+    }
 
-  return <- match.arg(return)
-  stopifnot(length(to) == 1,
-            length(yr) == 1,
-            length(lat) == length(lon))
+    return <- match.arg(return)
+    stopifnot(length(to) == 1,
+              length(yr) == 1,
+              length(lat) == length(lon))
 
-  if (to != "STE" && !isNamespaceLoaded("ASGS")) {
-    shapefile <- get(paste0(to, "_", yr))
+    if (to != "STE" && !isNamespaceLoaded("ASGS")) {
+      shapefile <- get(paste0(to, "_", yr))
+    } else {
+      shapefile <- ASGS.foyer::STE_2016_simple
+    }
   } else {
-    shapefile <- ASGS.foyer::STE_2016_simple
+    shapefile <- .shapefile
+    stopifnot("proj4string" %in% methods::slotNames(shapefile),
+              "data" %in% methods::slotNames(shapefile))
   }
   if (!is(shapefile, "SpatialPolygonsDataFrame")) {
     stop("Attempted to retrieve `", paste0(to, "_", yr), "` internally ",
