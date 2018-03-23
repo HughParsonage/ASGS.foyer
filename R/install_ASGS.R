eac30d2e8898058121e0bb49ed77205052a5ed37
HughParsonage <hugh.parsonage@gmail.com>
2018-03-24 00:16:08 +1100

    Fix repos/type issue

--- R/install_ASGS.R
+++ R/install_ASGS.R
@@ -1,16 +1,36 @@
 #' Install a (nearly) complete package of the Australian Statistical Geography Standard
 #' @param temp.tar.gz A file to save the ASGS tarball. Since the package is quite large,
 #' it may be prudent to set this to a non-temporary file so that you can attempt reinstallation.
-#' @param ... Arguments passed to \code{\link[utils]{install.packages}}.
+#' @param repos,type Passed to \code{\link[utils]{install.packages}} when installing ASGS's dependencies (if not already installed).
+#' @param ... Other arguments passed to \code{\link[utils]{install.packages}}.
 #' @export
 
-install_ASGS <- function(temp.tar.gz = tempfile(fileext = ".tar.gz"), ...) {
+install_ASGS <- function(temp.tar.gz = tempfile(fileext = ".tar.gz"),
+                         repos = getOption("repos"),
+                         type = getOption("pkgType"),
+                         ...) {
   asgs_deps <-
     c("dplyr", "leaflet", "sp", "spedep", "htmltools", "magrittr", "methods",
       "rgdal", "data.table", "hutils")
 
+  dots2list <- function(...) {
+    eval(substitute(alist(...)))
+  }
+
+
   if (!all(vapply(asgs_deps, requireNamespace, quietly = TRUE, logical(1L)))) {
-    utils::install.packages(asgs_deps, ...)
+    r <- getOption("repos")
+    if (identical(r["CRAN"], "@CRAN@")) {
+      message("Setting CRAN repository to https://rstudio.cran.com")
+      utils::install.packages(asgs_deps,
+                              repos = "https://rstudio.cran.com",
+                              type = type)
+    } else {
+      utils::install.packages(asgs_deps,
+                              repos = repos,
+                              type = type,
+                              ...)
+    }
   }
 
   message("Attempting install of ASGS (700 MB) from Dropbox. This should take some minutes to download.")
--- tests/testthat/test-install_asgs.R
+++ tests/testthat/test-install_asgs.R
@@ -4,3 +4,8 @@ test_that("Installation OK", {
   skip_on_cran()
   install_ASGS()
 })
+
+test_that("Installation when using repos OK", {
+  skip_on_cran()
+  install_ASGS(repos = "https://mran.microsoft.com/snapshot/2018-01-01")
+})
