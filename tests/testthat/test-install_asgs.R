2b820f663033021704807fccd88f8c42767f625a
HughParsonage <hugh.parsonage@gmail.com>
2018-03-24 02:42:06 +1100

    Try explicit tests

--- tests/testthat/test-install_asgs.R
+++ tests/testthat/test-install_asgs.R
@@ -3,9 +3,11 @@ context("test-install_asgs.R")
 test_that("Installation OK", {
   skip_on_cran()
   install_ASGS()
+  expect_true("ASGS" %in% rownames(installed.packages()))
 })
 
 test_that("Installation when using repos OK", {
   skip_on_cran()
   install_ASGS(repos = "https://mran.microsoft.com/snapshot/2018-01-01")
+  expect_true("ASGS" %in% rownames(installed.packages()))
 })
