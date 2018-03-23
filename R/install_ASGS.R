1be1450f2d8a0a20d30244c6fa60357043138139
HughParsonage <hugh.parsonage@gmail.com>
2018-03-24 01:38:55 +1100

    Correct typo in ASGS dependencies

--- R/install_ASGS.R
+++ R/install_ASGS.R
@@ -10,7 +10,7 @@ install_ASGS <- function(temp.tar.gz = tempfile(fileext = ".tar.gz"),
                          type = getOption("pkgType"),
                          ...) {
   asgs_deps <-
-    c("dplyr", "leaflet", "sp", "spedep", "htmltools", "magrittr", "methods",
+    c("dplyr", "leaflet", "sp", "spdep", "htmltools", "magrittr", "methods",
       "rgdal", "data.table", "hutils")
 
   dots2list <- function(...) {
