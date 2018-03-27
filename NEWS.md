7e4518ec88b8414fa56adb51633ca9b9cadf4632
HughParsonage <hugh.parsonage@gmail.com>
2018-03-27 17:39:52 +1100

    Attempt reinstallation

--- NEWS.md
+++ NEWS.md
@@ -1,6 +1,6 @@
 # ASGS.foyer 0.2.0
 
-* `install_ASGS`: if one of ASGS's dependencies is not installed, `install_ASGS` now installs it rather than raising an error *after* the download has taken place.
+* `install_ASGS`: if one of ASGS's dependencies is not installed, `install_ASGS` now installs it rather than raising an error *after* the download has taken place. By default, the packages are installed then a second attempt at installation occurs 
 
 # ASGS.foyer 0.1.0
 
--- R/install_ASGS.R
+++ R/install_ASGS.R
@@ -3,22 +3,40 @@
 #' it may be prudent to set this to a non-temporary file so that you can attempt reinstallation.
 #' @param repos,type Passed to \code{\link[utils]{install.packages}} when installing ASGS's dependencies (if not already installed).
 #' @param ... Other arguments passed to \code{\link[utils]{install.packages}}.
+#' @param .reinstalls Number of times to attempt to install any (absent) dependencies of \code{ASGS}
+#' before aborting. Try restarting R rather than setting this number too high.
 #' @export
 
 install_ASGS <- function(temp.tar.gz = tempfile(fileext = ".tar.gz"),
                          repos = getOption("repos"),
                          type = getOption("pkgType"),
-                         ...) {
+                         ...,
+                         .reinstalls = 2L) {
   asgs_deps <-
-    c("dplyr", "leaflet", "sp", "spdep", "htmltools", "magrittr",
+    c("dplyr", "leaflet", "sp",
+      "spdep", "htmltools", "magrittr",
       "rgdal", "data.table", "hutils")
 
-  dots2list <- function(...) {
-    eval(substitute(alist(...)))
+  absent_deps <- function(deps = asgs_deps) {
+    asgs_deps[!vapply(deps, requireNamespace, quietly = TRUE, logical(1L))]
   }
 
+  # dots2list <- function(...) {
+  #   eval(substitute(alist(...)))
+  # }
 
-  if (!all(vapply(asgs_deps, requireNamespace, quietly = TRUE, logical(1L)))) {
+  reinstalls <- .reinstalls
+  backoff <- 2
+  while (reinstalls > 0L && length(absent_deps())) {
+    reinstalls <- reinstalls - 1L
+    backoff <- 2 * backoff
+    message("Attempting to install the following uninstalled dependencies of ASGS:",
+            absent_deps(), ".\n",
+            reinstalls, " reinstalls remaining.")
+    if (backoff > 10) {
+      message("Waiting ", backoff, " seconds before attempting to reinstallation.",
+              "Wait times double on each reattempt as a courtesy to repository maintainers.")
+    }
     r <- getOption("repos")
     if (identical(r["CRAN"], "@CRAN@")) {
       message("Setting CRAN repository to https://rstudio.cran.com")
@@ -40,6 +58,13 @@ install_ASGS <- function(temp.tar.gz = tempfile(fileext = ".tar.gz"),
         }
       }
     }
+  }
+
+  if (length(absent_deps())) {
+    stop("ASGS requires the following packages:\n\t",
+         paste0(length(absent_deps()), collapse = "\n\t"),
+         "\n",
+         "Attempts to install did not succeed. Aborting before (lengthy) download.")
   }
 
   message("Attempting install of ASGS (700 MB) from Dropbox. This should take some minutes to download.")
