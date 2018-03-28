948a7f2c627c8cc838d494af747d645951d53dcd
HughParsonage <hugh.parsonage@gmail.com>
2018-03-28 22:44:44 +1100

    Travis (#1)
    * Correct typo in ASGS dependencies
    
    * Require LaTeX
    
    * Try explicit tests
    
    * Run installation but do not check
    
    * Try after_failure to track down problem
    
    * Install packages
    
    * Don't install methods - emits warning
    
    * Install in a for loop
    
    * skip one of the installs
    
    * Attempt reinstallation
    
    * Slightly nicer format
    
    * Try different installation environment
    
    * Use default install script
    
    * Display the package, not the pkg index
    
    * Diagnosing
    
    * Maybe an actual repository would be ncie
    
    * Try open test
    
    * retry
    
    * Add roxygen2
    
    * Install properly?
    
    * Pass library option to install packages
    
    * Install ASGS
    
    * Add raste
    
    * Closes #2
    
    * Verify #2
    
    * Incl overwrite=TRUE, and return char v of tempf

--- tests/testthat/test-install_asgs.R
+++ tests/testthat/test-install_asgs.R
@@ -2,10 +2,25 @@ context("test-install_asgs.R")
 
 test_that("Installation OK", {
   skip_on_cran()
+  tempf <- tempfile("001")
+  dir.create(tempf)
   install_ASGS()
+  expect_true(TRUE)
 })
 
 test_that("Installation when using repos OK", {
   skip_on_cran()
-  install_ASGS(repos = "https://mran.microsoft.com/snapshot/2018-01-01")
+  skip_if(file.exists("abc.tar.gz"))
+  tempf <- tempfile("002")
+  dir.create(tempf)
+  install_ASGS(temp.tar.gz = "abc.tar.gz",
+               repos = "https://mran.microsoft.com/snapshot/2018-01-01",
+               lib = tempf,
+               quiet = TRUE)
+  file.exists("abc.tar.tz")
+  expect_true(file.remove("abc.tar.tz"))
+})
+
+test_that("All arguments have an effect", {
+  expect_null(codetools::checkUsage(install_ASGS, all = TRUE))
 })
